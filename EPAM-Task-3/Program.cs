e7863ec7379de7fd141d2786cf72f47335ddbb93
PAKAJl <dgolubev7890@gmail.com>
2020-12-30 21:31:57 +0300

    Initial commit

---
+++ EPAM-Task-3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EPAM_Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
